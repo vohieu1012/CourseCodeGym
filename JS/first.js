7142206b62b22be07c4a6e4723a9b698d64f6e4b
vohieu1012 <74808978+vohieu1012@users.noreply.github.com>
2021-12-24 13:31:31 +0700

    firstCommit

---
+++ JS/first.js
@@ -0,0 +1,9 @@
+// let number_1= Number(prompt( "Nhập vào số thứ nhất"));
+// let number_2= Number(prompt( "Nhập vào số thứ hai"));
+// let sum = number_1 +number_2 ;
+// document.write(sum);
+var i;
+var myCar = ["vinfast", "lexus","audi"];
+for( i in myCar) {
+    document.write(i);
+}
